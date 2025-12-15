8d647469b6eb98edeec9275e2343bd5c328996f0
lemon-shark-25 <196343170+lemon-shark-25@users.noreply.github.com>
2025-12-15 12:31:05 +0100

    Added more things to GraphManager

--- GraphManager.py
+++ GraphManager.py
@@ -1,3 +1,22 @@
-import networkx as ns
+import networkx as nx
+import Person
 
+class GraphManager():
+    def __init__(self):
+        time = 0
+        G = nx.Graph()
+
+    def loadPerson(self):
+        pass
+
+    def loadProvisional(self):
+        nodes = [] 
+        for i in range(9):
+            nodes[i] = Person(i, i*10 + 1)
+
+        self.G.add_nodes_from(nodes)
+
+        for i in range(9):
+            for j in range(9):
+                self.add_edge(i, j, weight=0.2)
 
